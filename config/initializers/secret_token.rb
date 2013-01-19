7be879e7660a7e6563be5ccf1a1337fa1abae26b
zozo123 <eliaz123@gmail.com>
2013-01-19 20:15:21 +0200

    try ruby on rails into github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryRails::Application.config.secret_token = '2d51d5e5372009f441aa72e7b7756eadf457dbad8a5ae261cbd4a8880913b9b81cb2f37eb00fbb08821df01af3dd88764247a5574b1c2671edca2e09e6321985'
